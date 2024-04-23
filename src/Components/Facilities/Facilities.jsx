d084ce5a141cd6a4ce9e4b7b6f779ccc7923e938
Dharmik Puri Goswami <dharmikpuri2004@gmail.com>
2024-04-23 14:48:54 +0530

    AOS Animation Added

--- src/Components/Facilities/Facilities.jsx
+++ src/Components/Facilities/Facilities.jsx
@@ -1,7 +1,35 @@
 import { Box, Divider, SimpleGrid, Text } from '@chakra-ui/react';
 import React from 'react'
 import CountUp from 'react-countup';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 const Facilities = () => {
+    AOS.init();
+
+  // You can also pass an optional settings object
+  // below listed default settings
+  AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+  
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  
+  });
     const data = [
         {
             val: 15,
@@ -22,7 +50,9 @@ const Facilities = () => {
 
     ]
     return (
-        <Box w={"80%"} m={"auto"} mt={"5rem"}>
+        <Box w={"80%"} m={"auto"} mt={"5rem"} data-aos="fade-up"
+        data-aos-easing="linear"
+        data-aos-duration="1500">
          <Box mt={"2rem"} mb={"2rem"}>
          <Divider  bg="gray.700" />
          </Box>
